f1b46af48627a91a60d83b7f47a92f330f906613
KyungsooLee <jmtkd9196@gmail.com>
2023-12-06 01:10:28 +0900

    feat: 버튼 뷰 구현

--- Week 1/Liver/Pre-AppleDeveloperAcademy-SwiftUI/Pre-AppleDeveloperAcademy-SwiftUI/View/ButtonView.swift
+++ Week 1/Liver/Pre-AppleDeveloperAcademy-SwiftUI/Pre-AppleDeveloperAcademy-SwiftUI/View/ButtonView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct ButtonView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            // action(기능)
+            print("버튼을 클릭했습니다.")
+        } label: {
+            // view(버튼을 나타낼 뷰)
+            Text("Button")
+        }
     }
 }
 
