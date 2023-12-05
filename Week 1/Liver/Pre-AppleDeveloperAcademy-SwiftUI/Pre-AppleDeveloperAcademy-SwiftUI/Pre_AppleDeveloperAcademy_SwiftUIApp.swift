dfd7d605c605a91c378c0143addf2b7238a708ed
KyungsooLee <jmtkd9196@gmail.com>
2023-12-05 19:42:08 +0900

    feat: 프로젝트 초기 셋팅

---
+++ Week 1/Liver/Pre-AppleDeveloperAcademy-SwiftUI/Pre-AppleDeveloperAcademy-SwiftUI/Pre_AppleDeveloperAcademy_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pre_AppleDeveloperAcademy_SwiftUIApp.swift
+//  Pre-AppleDeveloperAcademy-SwiftUI
+//
+//  Created by Kyungsoo Lee on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Pre_AppleDeveloperAcademy_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Week 1/Liver/Pre-AppleDeveloperAcademy-SwiftUI/Pre-AppleDeveloperAcademy-SwiftUIUITests/Pre_AppleDeveloperAcademy_SwiftUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Pre_AppleDeveloperAcademy_SwiftUIUITestsLaunchTests.swift
+//  Pre-AppleDeveloperAcademy-SwiftUIUITests
+//
+//  Created by Kyungsoo Lee on 12/5/23.
+//
+
+import XCTest
+
+final class Pre_AppleDeveloperAcademy_SwiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
