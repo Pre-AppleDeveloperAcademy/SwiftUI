05c5c317fa8d3226c984428ac86457ba09cabe93
KyungsooLee <jmtkd9196@gmail.com>
2023-12-06 01:10:15 +0900

    feat: 프로젝트 생성

---
+++ Week 1/Liver/Pre-AppleDeveloperAcademy-SwiftUI/Pre-AppleDeveloperAcademy-SwiftUI/View/ButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonView.swift
+//  Pre-AppleDeveloperAcademy-SwiftUI
+//
+//  Created by Kyungsoo Lee on 12/6/23.
+//
+
+import SwiftUI
+
+struct ButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonView()
+}
