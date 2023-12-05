e87b604f4388366fe5358782210b64f1eb5b91e1
KyungsooLee <jmtkd9196@gmail.com>
2023-12-06 01:36:03 +0900

    feat: ZStack 뷰 구현

--- Week 1/Liver/Pre-AppleDeveloperAcademy-SwiftUI/Pre-AppleDeveloperAcademy-SwiftUI/View/ZStackView.swift
+++ Week 1/Liver/Pre-AppleDeveloperAcademy-SwiftUI/Pre-AppleDeveloperAcademy-SwiftUI/View/ZStackView.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct ZStackView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // ZStack : Z축에 대한 스택에 뷰를 쌓는다.
+            // (더 나중에 쌓인 뷰가 제일 상단에 위치한다.)
+            Text("리버")
+                .foregroundStyle(Color.white)
+                .background(Color.gray)
+            Text("나기")
+                .foregroundStyle(Color.white)
+                .background(Color.blue)
+                .offset(x: 10, y: 10)
+            Text("구밥")
+                .foregroundStyle(Color.white)
+                .background(Color.black)
+                .offset(x: 20, y: 20)
+            Text("허니비")
+                .foregroundStyle(Color.white)
+                .background(Color.yellow)
+                .offset(x: 30, y: 30)
+        }
     }
 }
 
