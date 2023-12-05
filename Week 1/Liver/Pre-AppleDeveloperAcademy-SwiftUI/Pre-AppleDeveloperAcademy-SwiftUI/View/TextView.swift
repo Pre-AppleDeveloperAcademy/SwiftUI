c238a58995117a29f08aaf108772c833193fb3e0
KyungsooLee <jmtkd9196@gmail.com>
2023-12-06 01:11:45 +0900

    feat: 텍스트 뷰 구현

---
+++ Week 1/Liver/Pre-AppleDeveloperAcademy-SwiftUI/Pre-AppleDeveloperAcademy-SwiftUI/View/TextView.swift
@@ -0,0 +1,18 @@
+//
+//  TextView.swift
+//  Pre-AppleDeveloperAcademy-SwiftUI
+//
+//  Created by Kyungsoo Lee on 12/6/23.
+//
+
+import SwiftUI
+
+struct TextView: View {
+    var body: some View {
+        Text("Text View입니다.\n이런식의 줄바꿈도 가능합니다.")
+    }
+}
+
+#Preview {
+    TextView()
+}
